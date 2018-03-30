245bc92890a9dceca14bc3d2e519290deddb6209
budi1200 <alenab22@gmail.com>
2018-03-30 10:21:46 +0200

    require login to access certain pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :require_login
+  
   private
 
   def logged_in?
       @Current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def require_login
+    unless logged_in?
+      redirect_to login_path
+    end
   end
 
   helper_method :logged_in?
